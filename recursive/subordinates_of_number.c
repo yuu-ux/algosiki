509586848ecf7a3eefa871d0a826ada6a22a3218
yuu-ux <yuugaehara@gmail.com>
2024-09-04 17:07:21 +0900

    feat: add subordinates_of_number
    ==== Prefix ====
    - feat: 新しい機能
    - fix: バグの修正
    - docs: ドキュメントのみの変更
    - style: 空白、フォーマット、セミコロン追加など
    - refactor: 仕様に影響がないコード改善(リファクタ)
    - perf: パフォーマンス向上関連
    - test: テスト関連
    - chore: ビルド、補助ツール、ライブラリ関連
    - wip: 一時中断

--- recursive/subordinates_of_number.c
+++ recursive/subordinates_of_number.c
@@ -1,10 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int is_staff(int X, int **staffs, int *staff_count)
+{
+    int result;
+    int staff;
+
+    result = 0;
+    for (int i = 0; i < staff_count[X]; i++)
+    {
+        staff = staffs[X][i];
+        result += is_staff(staff, staffs, staff_count) + 1;
+    }
+    return result;
+}
 
 int main(void)
 {
     int N;
     int X;
+    int *P;
+    int **staffs;
+    int *staff_count;
+    int manager;
 
     scanf("%d %d", &N, &X);
+    P = (int *)malloc(sizeof(int) * N);
 
+    // 入力を受け取る
+    for (int i = 1; i < N; i++)
+        scanf("%d", &P[i]);
+
+    // 部下を格納する
+    staffs = (int **)malloc(sizeof(int *) * N);
+    // 部下の人数を数える
+    staff_count = (int *)calloc(N, sizeof(int));
+
+    for (int i = 0; i < N; i++)
+        staffs[i] = (int *)malloc(sizeof(int) * N);
+    // マネージャーと部下の2次元配列を作る
+    // それと同時に各マネージャーに何人部下がいるのかをカウントする
+    for (int i = 1; i < N; i++)
+    {
+        manager = P[i];
+        staffs[manager][staff_count[manager]++] = i;
+    }
+
+    printf("%d\n", is_staff(X, staffs, staff_count));
+    for (int i = 0; i < N; i++)
+        free(staffs[i]);
+    free(staffs);
+    free(staff_count);
+    free(P);
+
+    return 0;
 }
