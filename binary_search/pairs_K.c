9a392a5f0d0cc060185469a286e1b4089ac2c43b
yuu-ux <yuugaehara@gmail.com>
2024-09-15 15:51:39 +0900

    feat: add pairs_K.c
    ==== Prefix ====
    - feat: 新しい機能
    - fix: バグの修正
    - docs: ドキュメントのみの変更
    - style: 空白、フォーマット、セミコロン追加など
    - refactor: 仕様に影響がないコード改善(リファクタ)
    - perf: パフォーマンス向上関連
    - test: テスト関連
    - chore: ビルド、補助ツール、ライブラリ関連
    - wip: 一時中断

--- binary_search/pairs_K.c
+++ binary_search/pairs_K.c
@@ -22,19 +22,30 @@ int main(void)
     // 昇順にソートする
     qsort(A, N, sizeof(int), compare);
 
+    //二分探索スタート
     long long count = 0;
+    int left;
+    int right;
+
+    // 外側ループは要素数分繰り返す
     for (int i = 0; i < N; i++)
     {
-        int left = 0;
-        int right = N;
+        // 各要素数初めから終わりまでチェックするために、初期化する
+        left = 0;
+        right = N-1;
+        // 内側ループで条件を満たす最小の値を確定させる
         while (left != right)
         {
-            int mid = (left + right) / 2;
+            int mid = (right + left) / 2;
+            // 条件を満たすなら今チェックしてる値も含めて右側を更新する
+            // 条件を満たさないなら今チェックしてる値の次から探索したいため、leftをmid+1して探索範囲を狭める
             if (A[mid] >= K - A[i])
                 right = mid;
             else
                 left = mid + 1;
         }
+        // 組み合わせの数は各要素の条件を満たす最小の値のインデックス値になる。
+        // 条件を満たす最小の値より大きい要素は全て条件を満たすため、N-条件を満たす最小のインデックス値で条件を満たす組み合わせがいくつあるか求められる。
         count += N - left;
     }
     printf("%lld\n", count);
