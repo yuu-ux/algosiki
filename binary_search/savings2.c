d5bca1d176701da2df0bd84939054cf60db569aa
yuu-ux <yuugaehara@gmail.com>
2024-09-17 21:07:57 +0900

    refactor: reimplementation ==== Prefix ==== - feat: 新しい機能 - fix: バグの修正 - docs: ドキュメントのみの変更 - style: 空白、フォーマット、セミコロン追加など - refactor: 仕様に影響がないコード改善(リファクタ) - perf: パフォーマンス向上関連 - test: テスト関連 - chore: ビルド、補助ツール、ライブラリ関連 - wip: 一時中断

--- binary_search/savings2.c
+++ binary_search/savings2.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long f(long long x) {
+long    f(long x)
+{
     return x * (x + 1) / 2;
 }
 
-int main(void) {
+int main(void)
+{
     int N;
     scanf("%d", &N);
-    long long *X = (long long *)malloc(sizeof(long long) * N);
+    long *X = (long *)malloc(sizeof(long) * N);
     for (int i = 0; i < N; i++)
-        scanf("%lld", &X[i]);
+        scanf("%ld", &X[i]);
 
-    for (int i = 0; i < N; i++) {
-        long long left = 1;
-        long long right = 2e9; // 適切な上限値
-        while (left < right) {
-            long long mid = left + (right - left) / 2;
+    for (int i = 0; i < N; i++)
+    {
+        long left = 0;
+        long right = 2000000000;
+        while (left < right)
+        {
+            long mid = left + (right - left) / 2;
             if (f(mid) >= X[i])
                 right = mid;
             else
                 left = mid + 1;
         }
-        printf("%lld\n", left);
+        printf("%ld\n", left);
     }
-    free(X);
-    return 0;
 }
